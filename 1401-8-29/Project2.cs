243e3dca874b72a0501b37e0897eddb2739bacbe
Kianmehr Kimiavi <85435062+KianmehrKM@users.noreply.github.com>
2022-12-10 15:35:00 +0330

    Update Project2.cs

--- 1401-8-29/Project2.cs
+++ 1401-8-29/Project2.cs
@@ -6,22 +6,42 @@ namespace Proj2
     {
         static void Main(string[] args)
         {
+            // Dar inja yek moteghayer dar naza gerefte shode ast
             float a;
+            // A = Adad
 
+            // Dar inja, az karbar darkhast mishe ke yek adad vared konad
+            // Va adade vared shode dar moteghayere "A" gozashte mishe
             Console.Write("Adad ra vared konid: ");
             a = float.Parse(Console.ReadLine());
 
+            // Agar adade dade shode, kamtar az 0 bood
+            // Yani inke adade manfi dade shod
+            // Payami chap mikone ke mige adad manfiye
             if (a < 0)
-                Console.WriteLine("manfi");
+                Console.WriteLine("Adad manfi ast");
+            
+            // Vali agar adad beine 0 ta 9 bood
+            // Chap kone ke adad yek raghami ast
             else if ((a >= 0) && (a <= 9))
-                Console.WriteLine("1 raghami");
+                Console.WriteLine("Adad yek raghami ast");
+            
+            // Vali agar adade dade shode beine 10 ta 99 bood
+            // Chap kone ke adad do raghami mi bashad
             else if ((a > 9) && (a <= 99))
-                Console.WriteLine("2 raghami");
+                Console.WriteLine("Adad do raghami ast");
+            
+            // Vali agar adade dade shode beine 100 ta 999 bood
+            // Chap kone ke adade dade shode se raghami ast
             else if ((a > 99) && (a <= 999))
-                Console.WriteLine("3 raghami");
+                Console.WriteLine("Adad se raghami ast");
+            
+            // Vali agar bishtar az 999 bood
+            // Chap kone ke in adad bishtar az se ragham ast
             else if (a > 1000)
-                Console.WriteLine("bish az 3 ragham");
+                Console.WriteLine("Adad bishtar az se ragham ast");
 
+            // Payane barnameh
             Console.ReadKey();
         }
     }
