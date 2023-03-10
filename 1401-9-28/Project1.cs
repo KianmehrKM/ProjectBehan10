affc62d0c6ea5261213786474b4ac8f3dce8f07e
Kianmehr Kimiavi <85435062+KianmehrKM@users.noreply.github.com>
2023-03-10 17:32:27 +0330

    Update Project1.cs

--- 1401-9-28/Project1.cs
+++ 1401-9-28/Project1.cs
@@ -6,23 +6,34 @@ namespace ConsoleApp31
     {
         static void Main(string[] args)
         {
+            // Moteghayer ma inja be esme "COLOR" moarefi shode
+            // Ke az jense "char" hastesh
+            // "char" dar inja yani ye character migire
             char color;
 
+            // Peygham mide, mige ke ye harf vared kon
+            // Harfi ke vared shode ro mikoone, ke az jense "char" hastesh
+            // Baad mizare tooye "COLOR"
             Console.WriteLine("Enter a letter (R/G/B): ");
             color = char.Parse(Console.ReadLine());
 
+            // Hala migim agar R koochik ya bozorg bood
+            // Rang text ro ghermez kone va bad bege ke ghermez hastesh
             if ((color == 'R')||(color=='r'))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("It's Red!");
             }
 
+            // Vali agar G koochik ya bozorg bood
+            // Rang text ro sabz kone va bad bege ke sabz hastesh 
             else if ((color == 'G')||(color=='g'))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("It's Green!");
             }
 
+            //
             else if ((color=='B')||(color=='b'))
             {
                 Console.ForegroundColor = ConsoleColor.Blue;
